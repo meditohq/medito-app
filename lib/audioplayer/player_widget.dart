235c6aa142eaca804ee926074c23ade7dc66844f
MSpeed <michaelcspeed+git@gmail.com>
2020-02-20 19:03:35 +0100

    fixe READ MORE padding

--- lib/audioplayer/player_widget.dart
+++ lib/audioplayer/player_widget.dart
@@ -195,8 +195,7 @@ class _PlayerWidgetState extends State<PlayerWidget> {
         Expanded(
           flex: 1,
           child: Padding(
-            padding: const EdgeInsets.only(
-                top: 8.0, bottom: 8.0, left: 24.0, right: 24.0),
+            padding: const EdgeInsets.only(bottom: 8.0, left: 24.0, right: 24.0),
             child: OutlineButton(
               child: Text(
                 "READ MORE",
