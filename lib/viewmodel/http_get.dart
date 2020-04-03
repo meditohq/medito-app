972fda49584d3daf66c4372c626b70db6841cf2d
MSpeed <michaelcspeed+git@gmail.com>
2020-04-03 14:04:32 +0200

    cache improvements

--- lib/viewmodel/http_get.dart
+++ lib/viewmodel/http_get.dart
@@ -1,13 +1,26 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:Medito/utils/utils.dart';
 import 'package:http/http.dart' as http;
 
 import 'auth.dart';
 import 'cache.dart';
 
 Future httpGet(String url, {bool skipCache = false}) async {
-  var cache = skipCache ? null : await readJSONFromCache(url);
+
+  var cache;
+
+  if (skipCache) {
+    cache = null;
+  } else {
+    cache = await readJSONFromCache(url);
+  }
+
+  if(cache == null && ! await checkConnectivity()){
+    cache = await readJSONFromCache(url);
+  }
+
   if (cache == null) {
     final response = await http.get(
       url,
