c8060d9424a85b8446128906241b999b0ae2f567
MSpeed <michaelcspeed+git@gmail.com>
2020-04-12 19:19:15 +0200

    tap back at top of screen to go back from loading widget

--- lib/widgets/loading_list_widget.dart
+++ lib/widgets/loading_list_widget.dart
@@ -6,6 +6,21 @@ import 'package:flutter/material.dart';
 class LoadingListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        getListView(),
+        GestureDetector(
+          onTap: () => _pop(context),
+          child: Container(
+            height: 200,
+            color: Colors.transparent,
+          ),
+        ),
+      ],
+    );
+  }
+
+  ListView getListView() {
     return ListView.builder(
         itemCount: 6,
         itemBuilder: (context, i) {
@@ -13,10 +28,14 @@ class LoadingListWidget extends StatelessWidget {
             var list = [
               ListItem("            ", "             ", null, parentId: "...")
             ];
-            return IgnorePointer(ignoring: true, child: NavPillsWidget(list: list));
+            return IgnorePointer(
+                ignoring: true, child: NavPillsWidget(list: list));
           }
           return LoadingItemWidget(index: i + 1);
         });
   }
 
+  void _pop(BuildContext context) {
+    Navigator.pop(context);
+  }
 }
