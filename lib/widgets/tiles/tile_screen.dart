7bb2afb2cdac71c1ce4540dabe7bab7c8cf26c23
MSpeed <michaelcspeed+git@gmail.com>
2020-05-08 15:06:47 +0200

    changed name of folder nav file

--- lib/widgets/tiles/tile_screen.dart
+++ lib/widgets/tiles/tile_screen.dart
@@ -29,7 +29,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../folder_nav.dart';
+import '../folder_nav_widget.dart';
 import '../streak_tiles_utils.dart';
 
 class TileList extends StatefulWidget {
