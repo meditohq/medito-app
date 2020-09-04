1cf5d2165543c27cddcd0198f5f6b50233a046d6
Yash <yash.halgaonkar18@vit.edu>
2020-09-04 18:37:19 +0530

    added new google-services.json file. Changed the strings in tracking.dart

--- lib/tracking/tracking.dart
+++ lib/tracking/tracking.dart
@@ -17,40 +17,40 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 
 class Tracking {
-  static const String SCREEN_LOADED = "screen loaded ";
-  static const String FOLDER_TAPPED = "folder tapped ";
-  static const String FILE_TAPPED = "file tapped ";
-  static const String PLAYER_TAPPED = "player tapped ";
-  static const String BOTTOM_SHEET = "bottom sheet open ";
-  static const String READ_MORE_TAPPED = "read more tapped ";
-  static const String BREADCRUMB = "breadcrumb ";
-  static const String TILE = "tile ";
-  static const String PLAYER = "player ";
+  static const String SCREEN_LOADED = "screen_loaded";
+  static const String FOLDER_TAPPED = "folder_tapped";
+  static const String FILE_TAPPED = "file_tapped";
+  static const String PLAYER_TAPPED = "player_tapped";
+  static const String BOTTOM_SHEET = "bottom_sheet_open";
+  static const String READ_MORE_TAPPED = "read_more_tapped";
+  static const String BREADCRUMB = "breadcrumb";
+  static const String TILE = "tile";
+  static const String PLAYER = "player";
 
-  static const String AUDIO_DOWNLOAD = "audio download ";
-  static const String AUDIO_PLAY = "audio started ";
-  static const String AUDIO_ERROR = "audio error ";
-  static const String AUDIO_REWIND = "audio rewind ";
-  static const String AUDIO_FF = "audio forward ";
-  static const String AUDIO_RESUME = "audio reumed ";
-  static const String AUDIO_STOPPED = "audio stopped ";
-  static const String AUDIO_PAUSED = "audio paused ";
-  static const String AUDIO_COMPLETED = "audio completed ";
-  static const String AUDIO_SEEK = "audio seek to ";
+  static const String AUDIO_DOWNLOAD = "audio_download";
+  static const String AUDIO_PLAY = "audio_started";
+  static const String AUDIO_ERROR = "audio_error";
+  static const String AUDIO_REWIND = "audio_rewind";
+  static const String AUDIO_FF = "audio_forward";
+  static const String AUDIO_RESUME = "audio_resumed";
+  static const String AUDIO_STOPPED = "audio_stopped";
+  static const String AUDIO_PAUSED = "audio_paused";
+  static const String AUDIO_COMPLETED = "audio_completed";
+  static const String AUDIO_SEEK = "audio_seek_to";
 
-  static const String AUDIO_OPENED = "audio opened. ";
-  static const String FOLDER_OPENED = "folder opened. ";
-  static const String TEXT_ONLY_OPENED = "text only opened. ";
-  static const String AUDIO_AND_TEXT_OPENED = "audio and text file opened. ";
+  static const String AUDIO_OPENED = "audio_opened";
+  static const String FOLDER_OPENED = "folder_opened";
+  static const String TEXT_ONLY_OPENED = "text_only_opened";
+  static const String AUDIO_AND_TEXT_OPENED = "audio_and_text_file_opened";
   static const String CURRENTLY_SELECTED_FILE = "current file. ";
 
-  static const String BREADCRUMB_TAPPED = "breadcrumb tapped. ";
-  static const String PLAYER_BREADCRUMB_TAPPED = "close player. ";
-  static const String TILE_TAPPED = "tile tapped. ";
-  static const String FINDER = "finder widget. ";
-  static const String HOME = "home page. ";
-  static const String APP_CLOSED = "app closed. ";
-  static const String BACK_PRESSED = "back pressed. ";
+  static const String BREADCRUMB_TAPPED = "breadcrumb_tapped";
+  static const String PLAYER_BREADCRUMB_TAPPED = "close_player";
+  static const String TILE_TAPPED = "tile_tapped";
+  static const String FINDER = "finder_widget";
+  static const String HOME = "home_page";
+  static const String APP_CLOSED = "app_closed";
+  static const String BACK_PRESSED = "back_pressed";
   static FirebaseAnalytics _firebaseAnalytics;
   static FirebaseAnalyticsObserver _firebaseAnalyticsObserver;
 
@@ -60,6 +60,13 @@ class Tracking {
 
     _firebaseAnalytics = FirebaseAnalytics();
     _firebaseAnalyticsObserver = FirebaseAnalyticsObserver(analytics: _firebaseAnalytics);
+
+    //dummy event
+    _firebaseAnalytics.logEvent(
+      name: "tracker_initialized",
+      parameters: {},
+    );
+    print('tracking initialized');
   }
 
   static FirebaseAnalyticsObserver getObserver() => _firebaseAnalyticsObserver;
@@ -75,5 +82,7 @@ class Tracking {
         "action": action,
       },
     );
+
+    print("Event logges: $widgetName");
   }
 }
