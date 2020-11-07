86889ccf95354bd1ea10e5bfb0cae28f8e987aa4
Nipun Aggarwal <naggarwal2@atlassian.com>
2020-11-07 17:02:39 +0530

    Created Rating widget

---
+++ lib/widgets/in_app_review_widget.dart
@@ -0,0 +1,43 @@
+import 'package:Medito/utils/utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
+
+class InAppReviewWidget extends StatelessWidget {
+  BuildContext get context => this.context;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        child: new Center(
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new FloatingActionButton(
+                onPressed: rateUsPopUp,
+                child: new Icon(Icons.thumb_up, color: Colors.black12,),
+                backgroundColor: Colors.white,),
+
+              new FloatingActionButton(
+                onPressed: thanksPopUp,
+                child: new Icon(Icons.thumb_down, color: Colors.black12,),
+                backgroundColor: Colors.white,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  void rateUsPopUp() async {
+    final InAppReview inAppReview = InAppReview.instance;
+
+    if (await inAppReview.isAvailable()) {
+    inAppReview.requestReview();
+    }
+  }
+  void thanksPopUp() {
+    createSnackBarWithColor("Thanks for the rating", context, Colors.black12);
+    Navigator.pop(context);
+  }
+}
