24822cf0774dadd4fd11d47db9417277dd5a7f04
MSpeed <michaelcspeed+git@gmail.com>
2020-11-19 18:24:28 +0100

    formatted code

--- lib/utils/shared_preferences_utils.dart
+++ lib/utils/shared_preferences_utils.dart
@@ -3,13 +3,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 void addIntToSF(file, key, value) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   file = file.toString();
-  prefs.setInt(file+'_'+key, value);
+  prefs.setInt(file + '_' + key, value);
 }
+
 Future<int> getIntValuesSF(file, key) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  file=file.toString();
-  return prefs.getInt(file+'_'+key) ?? 0;
+  file = file.toString();
+  return prefs.getInt(file + '_' + key) ?? 0;
 }
+
 void addCurrentDateToSF(key) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   key = key.toString();
@@ -20,7 +22,7 @@ void addCurrentDateToSF(key) async {
 
 Future<int> daysSinceDate(key) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  if(!prefs.containsKey(key)) return 365;
+  if (!prefs.containsKey(key)) return 365;
   DateTime savedDate = DateTime.fromMillisecondsSinceEpoch(prefs.get(key));
   Duration difference = DateTime.now().difference(savedDate);
   return difference.inDays;
--- lib/widgets/session_options_screen.dart
+++ lib/widgets/session_options_screen.dart
@@ -94,7 +94,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
 
     widget.data.then((d) {
       this._illustration =
-      d?.illustration != null ? d?.illustration?.first : null;
+          d?.illustration != null ? d?.illustration?.first : null;
       this._primaryColor = d?.primaryColor;
       this._title = d?.title;
       this._secondaryColor = d?.secondaryColor;
@@ -122,7 +122,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
         child: PlayerButton(
           onPressed: _onBeginTap,
           child:
-          SizedBox(width: 24, height: 24, child: getBeginButtonContent()),
+              SizedBox(width: 24, height: 24, child: getBeginButtonContent()),
           primaryColor: _primaryColor != null
               ? parseColor(_primaryColor)
               : MeditoColors.lightColor,
@@ -170,7 +170,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
                           ////////// spacer
                           !Platform.isIOS
                               ? buildTextHeaderForRow(
-                              'Available Offline $_availableOfflineIndicatorText')
+                                  'Available Offline $_availableOfflineIndicatorText')
                               : Container(),
                           !Platform.isIOS ? buildOfflineRow() : Container(),
                           Container(height: 80)
@@ -213,7 +213,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
                       CircularProgressIndicator(
                         value: 1,
                         valueColor:
-                        AlwaysStoppedAnimation<Color>(Colors.black12),
+                            AlwaysStoppedAnimation<Color>(Colors.black12),
                       ),
                       CircularProgressIndicator(
                         value: value,
@@ -231,7 +231,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
         width: 24,
         child: CircularProgressIndicator(
             valueColor:
-            AlwaysStoppedAnimation<Color>(parseColor(_secondaryColor))),
+                AlwaysStoppedAnimation<Color>(parseColor(_secondaryColor))),
       );
     } else {
       return Icon(Icons.play_arrow, color: parseColor(_secondaryColor));
@@ -239,7 +239,6 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
   }
 
   Future<void> _onBeginTap() {
-
     Tracking.trackEvent(Tracking.TAP, Tracking.PLAY_TAPPED, widget.id);
 
     if (downloadSingleton == null || !downloadSingleton.isValid())
@@ -288,9 +287,9 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
   Widget buildDescriptionText() {
     return _contentText.isNotEmpty
         ? Padding(
-      padding: const EdgeInsets.only(bottom: 20.0, left: 16, right: 16),
-      child: getDescriptionMarkdownBody(_contentText, context),
-    )
+            padding: const EdgeInsets.only(bottom: 20.0, left: 16, right: 16),
+            child: getDescriptionMarkdownBody(_contentText, context),
+          )
         : Container();
   }
 
@@ -424,17 +423,18 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
     _offlineSelected = await checkFileExists(currentFile) ? 1 : 0;
     if (mounted)
       setState(() {
-        filterLengthsForThisPerson(voiceList[voiceSelected], clockTimeToDuration(lengthList[lengthSelected]).inMinutes);
+        filterLengthsForThisPerson(voiceList[voiceSelected],
+            clockTimeToDuration(lengthList[lengthSelected]).inMinutes);
         _updateAvailableOfflineIndicatorText();
       });
   }
 
   Future<void> onSessionPillTap(bool value, int index) async {
     filesList.forEach((file) => {
-      if (file.length == (lengthList[index]) &&
-          file.voice == (voiceList[voiceSelected]))
-        currentFile = file
-    });
+          if (file.length == (lengthList[index]) &&
+              file.voice == (voiceList[voiceSelected]))
+            currentFile = file
+        });
     _offlineSelected = await checkFileExists(currentFile) ? 1 : 0;
     setState(() {
       lengthSelected = index;
@@ -508,7 +508,8 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
     }
   }
 
-  void filterLengthsForThisPerson(String voiceSelected, [int previousMusicSelected]) {
+  void filterLengthsForThisPerson(String voiceSelected,
+      [int previousMusicSelected]) {
     lengthSelected = -1;
     lengthFilteredList.clear();
 
@@ -518,13 +519,12 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
       }
     });
     List<dynamic> roundedList;
-    for(int i = 0; i < lengthList.length; i++) {
+    for (int i = 0; i < lengthList.length; i++) {
       roundedList.add(clockTimeToDuration(lengthList[i]).inMinutes);
     }
-    if(previousMusicSelected!=null) lengthSelected = roundedList.indexOf(previousMusicSelected);
-    if(lengthSelected==-1) lengthSelected = 0;
-
-
+    if (previousMusicSelected != null)
+      lengthSelected = roundedList.indexOf(previousMusicSelected);
+    if (lengthSelected == -1) lengthSelected = 0;
   }
 
   Widget buildOfflineRow() {
@@ -688,7 +688,7 @@ class _SessionOptionsScreenState extends State<SessionOptionsScreen> {
     if (_offlineSelected != 0) {
       var time = clockTimeToDuration(lengthList[lengthSelected]).inMinutes;
       _availableOfflineIndicatorText =
-      '(${voiceList[voiceSelected]} - $time min)';
+          '(${voiceList[voiceSelected]} - $time min)';
     } else {
       _availableOfflineIndicatorText = "";
     }
