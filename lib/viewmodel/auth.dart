17a43d2e73a089eaa4faf7cc802c13b8a5395ce0
MSpeed <michaelcspeed+git@gmail.com>
2020-11-21 19:26:16 +0100

    add auth file so it compiles

---
+++ lib/viewmodel/auth.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+var basicAuth = 'Basic xxx==';
+
+var appId = Platform.isIOS
+    ? '1:xxx:ios:xxx'
+    : '1:xxx:android:xxx';
+
+var apiKey =
+    'xx:xxxx-xxx';
+
+var messagingSenderId = 'xxxx';
+
+var projectId = 'xx-xx';
+
+var databaseURL = 'https://xxx-xx.xxx.com';
