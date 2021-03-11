d1789b299e50b5c35778fe9f90fb10c8b27c0c7f
Michael Speed <michaelcspeed@gmail.com>
2021-03-11 18:47:45 +0100

    don't reshow dismissed announcements

---
+++ lib/widgets/packs/expand_animate_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ExpandedSection extends StatefulWidget {
+
+  final Widget child;
+  final bool expand;
+  ExpandedSection({this.expand = false, this.child});
+
+  @override
+  _ExpandedSectionState createState() => _ExpandedSectionState();
+}
+
+class _ExpandedSectionState extends State<ExpandedSection> with SingleTickerProviderStateMixin {
+  AnimationController expandController;
+  Animation<double> animation; 
+
+  @override
+  void initState() {
+    super.initState();
+    prepareAnimations();
+    _runExpandCheck();
+  }
+
+  ///Setting up the animation
+  void prepareAnimations() {
+    expandController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500)
+    );
+    animation = CurvedAnimation(
+      parent: expandController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _runExpandCheck() {
+    if(widget.expand) {
+      expandController.forward();
+    }
+    else {
+      expandController.reverse();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ExpandedSection oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _runExpandCheck();
+  }
+
+  @override
+  void dispose() {
+    expandController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      axisAlignment: 1.0,
+      sizeFactor: animation,
+      child: widget.child
+    );
+  }
+}
