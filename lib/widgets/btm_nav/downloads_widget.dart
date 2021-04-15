e4606a186076331f2c5df2ab2ed0598402f0453c
Mohit Agarwal <mohit.agarwal@helpshift.com>
2021-04-15 18:58:49 +0530

    Update UI as [er design for Empty Downloads and Favorites

--- lib/widgets/btm_nav/downloads_widget.dart
+++ lib/widgets/btm_nav/downloads_widget.dart
@@ -3,10 +3,13 @@ import 'package:Medito/network/folder/folder_reponse.dart';
 import 'package:Medito/utils/colors.dart';
 import 'package:Medito/utils/duration_ext.dart';
 import 'package:Medito/utils/navigation.dart';
+import 'package:Medito/utils/strings.dart';
+import 'package:Medito/widgets/empty_widget.dart';
 import 'package:Medito/widgets/folders/folder_list_item_widget.dart';
 import 'package:Medito/widgets/player/player_widget.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:pedantic/pedantic.dart';
 
 class DownloadsListWidget extends StatefulWidget {
@@ -20,12 +23,13 @@ class _DownloadsListWidgetState extends State<DownloadsListWidget>
 
   final key = GlobalKey<AnimatedListState>();
 
-  ValueNotifier<List<MediaItem>> downloadedSession = DownloadsBloc.downloadedSessions;
+  ValueNotifier<List<MediaItem>> downloadedSession =
+      DownloadsBloc.downloadedSessions;
 
   @override
   void initState() {
     super.initState();
-    DownloadsBloc.fetchDownloads().then((value){
+    DownloadsBloc.fetchDownloads().then((value) {
       if (value.isNotEmpty) {
         showSwipeToDeleteTip();
       }
@@ -56,29 +60,12 @@ class _DownloadsListWidgetState extends State<DownloadsListWidget>
         });
   }
 
-  Widget _getEmptyWidget() => Center(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: 300,
-            height: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.tag_faces,
-                  color: MeditoColors.walterWhite,
-                ),
-                Container(
-                  height: 32,
-                ),
-                Text(
-                  'From now on, your downloads will appear here',
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
+  Widget _getEmptyWidget() => EmptyStateWidget(
+        message: EMPTY_DOWNLOADS_MESSAGE,
+        image: SvgPicture.asset(
+          'assets/images/downloads.svg',
+          height: 168,
+          width: 178,
         ),
       );
 
@@ -145,27 +132,27 @@ class _DownloadsListWidgetState extends State<DownloadsListWidget>
 
   void showSwipeToDeleteTip() async {
     await _bloc.seenTip().then((seen) {
-        if (!seen) {
-          unawaited(_bloc.setSeenTip());
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.info_outline,
-                    color: MeditoColors.walterWhite,
-                  ),
-                  Container(
-                    width: 16,
-                    height: 10,
-                  ),
-                  Text('Swipe away a session to delete it')
-                ],
-              ),
+      if (!seen) {
+        unawaited(_bloc.setSeenTip());
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.info_outline,
+                  color: MeditoColors.walterWhite,
+                ),
+                Container(
+                  width: 16,
+                  height: 10,
+                ),
+                Text('Swipe away a session to delete it')
+              ],
             ),
-          );
-        }
-        ;
-      });
-   }
+          ),
+        );
+      }
+      ;
+    });
+  }
 }
