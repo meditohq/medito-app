0c5426c3a34a6d23b91040d2571bbc2513394411
Michael Speed <michaelcspeed@gmail.com>
2021-04-17 14:25:35 +0200

    pull to refresh on message widget

--- lib/widgets/home/daily_message_widget.dart
+++ lib/widgets/home/daily_message_widget.dart
@@ -1,12 +1,24 @@
 import 'package:Medito/network/api_response.dart';
 import 'package:Medito/network/home/daily_message_bloc.dart';
 import 'package:Medito/network/home/daily_message_response.dart';
+import 'package:Medito/utils/text_themes.dart';
 import 'package:Medito/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
-class DailyMessageWidget extends StatelessWidget {
+class DailyMessageWidget extends StatefulWidget {
+  @override
+  DailyMessageWidgetState createState() => DailyMessageWidgetState();
+
+  DailyMessageWidget({Key key}) : super(key: key);
+}
+
+class DailyMessageWidgetState extends State<DailyMessageWidget> {
   final _bloc = DailyMessageBloc();
+
+  void refresh() {
+    _bloc.fetchMessage(skipCache: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +26,11 @@ class DailyMessageWidget extends StatelessWidget {
         stream: _bloc.coursesList.stream,
         builder: (context, snapshot) {
           var widget;
+
+          if (!snapshot.hasData) {
+            return Container();
+          }
+
           switch (snapshot.data.status) {
             case Status.LOADING:
               widget = const CircularProgressIndicator();
@@ -29,7 +46,8 @@ class DailyMessageWidget extends StatelessWidget {
                     child: MarkdownBody(
                       data: snapshot.data.body.body,
                       onTapLink: launchUrl,
-                    ), // styleSheetTheme: Theme.of(context).textTheme.bodyText1),
+                      styleSheet: buildMarkdownStyleSheet(context),
+                    ),
                   ),
                 ],
               );
