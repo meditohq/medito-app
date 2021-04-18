4b62d981385300505e49cf376bfd23e8d192c70c
Michael Speed <michaelcspeed@gmail.com>
2021-04-18 18:03:31 +0200

    added ULA

--- lib/widgets/packs/pack_list_item.dart
+++ lib/widgets/packs/pack_list_item.dart
@@ -1,4 +1,18 @@
-import 'package:Medito/utils/colors.dart';
+/*This file is part of Medito App.
+
+Medito App is free software: you can redistribute it and/or modify
+it under the terms of the Affero GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Medito App is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Affero GNU General Public License for more details.
+
+You should have received a copy of the Affero GNU General Public License
+along with Medito App. If not, see <https://www.gnu.org/licenses/>.*/
+
 import 'package:Medito/utils/utils.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +37,9 @@ class PackListItemWidget extends StatelessWidget {
               children: [
                 _getTitle(context),
                 Container(height: 4),
-                data.subtitle.isNotEmptyAndNotNull() ? _getSubtitle(context) : Container()
+                data.subtitle.isNotEmptyAndNotNull()
+                    ? _getSubtitle(context)
+                    : Container()
               ],
             ),
           ),
