25ff35d8c1b8054871edbead947e2602b88fb37a
Tien Nguyen <nqtien.dev@gmail.com>
2021-05-14 17:03:51 +0200

    chore: improve animation on homescreen

--- lib/widgets/home/courses_row_widget.dart
+++ lib/widgets/home/courses_row_widget.dart
@@ -1,8 +1,7 @@
 import 'package:Medito/network/api_response.dart';
 import 'package:Medito/network/home/courses_bloc.dart';
 import 'package:Medito/network/home/courses_response.dart';
-import 'package:Medito/utils/colors.dart';
-import 'package:Medito/utils/utils.dart';
+import 'package:Medito/widgets/home/courses_row_item_widget.dart';
 import 'package:flutter/material.dart';
 
 class CoursesRowWidget extends StatefulWidget {
@@ -22,6 +21,12 @@ class CoursesRowWidgetState extends State<CoursesRowWidget> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _bloc.fetchCourses();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Padding(
@@ -33,35 +38,47 @@ class CoursesRowWidgetState extends State<CoursesRowWidget> {
             stream: _bloc.coursesList.stream,
             initialData: ApiResponse.loading(),
             builder: (context, snapshot) {
-              switch (snapshot.data.status) {
-                case Status.LOADING:
-                  return _getLoadingWidget();
-                  break;
-                case Status.COMPLETED:
-                  return _horizontalCoursesRow(snapshot);
-
-                  break;
-                case Status.ERROR:
-                  return Icon(Icons.error);
-                  break;
-              }
-              return Container();
+              return AnimatedSwitcher(
+                duration: Duration(milliseconds: 100),
+                child: _buildCoursesRowWidget(snapshot),
+              );
             }),
       ),
     ]);
   }
 
-  Widget _horizontalCoursesRow(
+  Widget _buildCoursesRowWidget(
       AsyncSnapshot<ApiResponse<CoursesResponse>> snapshot) {
+    switch (snapshot.data.status) {
+      case Status.LOADING:
+        return _horizontalCoursesRow(null, ValueKey('CoursesRowLoading'));
+      case Status.COMPLETED:
+        return _horizontalCoursesRow(snapshot, ValueKey('CoursesRowCompleted'));
+      case Status.ERROR:
+        return Icon(Icons.error);
+      default:
+        return Container();
+    }
+  }
+
+  Widget _horizontalCoursesRow(
+      AsyncSnapshot<ApiResponse<CoursesResponse>> snapshot, Key key) {
     var list = <Widget>[Container(width: 16)];
-    snapshot.data?.body?.data?.forEach((element) {
-      list.add(CoursesRowItemWidget(
-        element,
-        onTap: widget.onTap,
-      ));
-    });
+    if (snapshot != null) {
+      snapshot.data?.body?.data?.forEach((element) {
+        list.add(CoursesRowItemWidget(
+          data: element,
+          onTap: widget.onTap,
+        ));
+      });
+    } else {
+      list.addAll(List.generate(4, (index) {
+        return CoursesRowItemWidget.waiting();
+      }));
+    }
 
     return SingleChildScrollView(
+      key: key,
       scrollDirection: Axis.horizontal,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -70,76 +87,4 @@ class CoursesRowWidgetState extends State<CoursesRowWidget> {
       ),
     );
   }
-
-  Widget _getLoadingWidget() => Row(
-        children: [
-          Container(width: 16),
-          Container(color: MeditoColors.moonlight, height: 132, width: 132),
-          Container(width: 16),
-          Container(color: MeditoColors.moonlight, height: 132, width: 132),
-        ],
-      );
-}
-
-class CoursesRowItemWidget extends StatelessWidget {
-  const CoursesRowItemWidget(this.data, {Key key, this.onTap})
-      : super(key: key);
-
-  final void Function(dynamic, dynamic) onTap;
-
-  final Data data;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap(data.type, data.id),
-      child: Container(
-        width: 148,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _imageStack(),
-            Container(height: 8),
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: Text(
-                data.title,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-            Container(height: 2),
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: Text(data.subtitle,
-                  style: Theme.of(context).textTheme.subtitle1),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Stack _imageStack() {
-    return Stack(
-      children: [
-        SizedBox(width: 132, height: 132, child: _buildCardBackground()),
-        Positioned.fill(
-          child: Center(
-            child: SizedBox(
-                width: 92,
-                height: 92,
-                child: getNetworkImageWidget(data.coverUrl)),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildCardBackground() => data.backgroundImage.isEmptyOrNull()
-      ? Container(
-          color: data.colorPrimary.isEmptyOrNull()
-              ? MeditoColors.moonlight
-              : parseColor(data.colorPrimary))
-      : getNetworkImageWidget(data.backgroundImageUrl);
 }
