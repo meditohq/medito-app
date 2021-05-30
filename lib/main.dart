0f112ad12ad21b1d34d6fa734b0efbf06227081a
Michael Speed <michaelcspeed@gmail.com>
2021-05-30 19:16:03 +0200

    update gradle, add --info to build command

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ import 'package:audio_service/audio_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pedantic/pedantic.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
