71660c63813d389ddb4e5238c10ae6db40be23cf
Michael Speed <michaelcspeed@gmail.com>
2021-06-08 22:50:53 +0200

    save bg sound option and replay

--- lib/utils/shared_preferences_utils.dart
+++ lib/utils/shared_preferences_utils.dart
@@ -1,5 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+const BG_SOUND_PREF = 'bg_sound_prefs';
+const BG_SOUND_PREF_NAME = 'bg_sound_prefs_name';
+
 void addIntToSF(file, key, value) async {
   var prefs = await SharedPreferences.getInstance();
   file = file.toString();
@@ -26,4 +29,20 @@ Future<int> daysSinceDate(key) async {
   var savedDate = DateTime.fromMillisecondsSinceEpoch(prefs.get(key));
   var difference = DateTime.now().difference(savedDate);
   return difference.inDays;
+}
+
+Future<void> addBgSoundSelectionToSharedPrefs(String file, String name) async {
+  var prefs = await SharedPreferences.getInstance();
+  await prefs.setString(BG_SOUND_PREF, file);
+  await prefs.setString(BG_SOUND_PREF_NAME, name);
+}
+
+Future<String> getBgSoundFileFromSharedPrefs() async {
+  var prefs = await SharedPreferences.getInstance();
+  return prefs.getString(BG_SOUND_PREF);
+}
+
+Future<String> getBgSoundNameFromSharedPrefs() async {
+  var prefs = await SharedPreferences.getInstance();
+  return prefs.getString(BG_SOUND_PREF_NAME);
 }
