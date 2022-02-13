3a101e6de2e29f50d1b40612d11cd39457c8d1f6
Michael Speed <michaelcspeed@gmail.com>
2022-02-13 16:30:17 +0100

    padding

--- lib/widgets/home/daily_message_item_widget.dart
+++ lib/widgets/home/daily_message_item_widget.dart
@@ -23,7 +23,7 @@ class DailyMessageItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(
-          left: 12.0, right: 12.0, bottom: 8.0, top: 32.0),
+          left: 12.0, right: 12.0, bottom: 8.0, top: 16.0),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
