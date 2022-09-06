dc50f897f92c480dfdb6402c72e5ab2ca201f6e1
Michael Speed <michaelcspeed@gmail.com>
2022-09-06 18:30:15 +0200

    update flutter

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mike/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mike/Projects/medito-app"
+export "FLUTTER_APPLICATION_PATH=/Users/mike/Projects/meditoapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.0.48"
 export "FLUTTER_BUILD_NUMBER=20048"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
