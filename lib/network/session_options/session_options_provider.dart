88b53f4ce0393a1378c33224ade7ffb4e9319113
Michael Speed <michaelcspeed@gmail.com>
2023-01-08 16:41:56 +0100

    Started session options provider

---
+++ lib/network/session_options/session_options_provider.dart
@@ -0,0 +1,43 @@
+import 'package:Medito/network/http_get.dart';
+import 'package:Medito/network/session_options/session_opts.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../auth.dart';
+
+part 'session_options_provider.g.dart';
+
+var ext = 'items/sessions/';
+var dailiesExt = 'items/dailies/';
+var parameters =
+    '?fields=*,author.body,audio.file.id,audio.file.voice,audio.file.length';
+var screen;
+
+@riverpod
+Future<SessionOptionsResponse?> sessionOptionsData(SessionOptionsDataRef ref,
+    {String? id, bool skipCache = false, bool isDaily = false}) async {
+  if (id == null) {
+    throw Exception('Folder ID is null!');
+  }
+
+  final content = await _httpGet(id, skipCache, isDaily);
+
+  if (content == null) return null;
+  return SessionOptionsResponse.fromJson(content);
+}
+
+Future<Map<String, Object?>?> _httpGet(
+    String id, bool skipCache, bool isDaily) {
+  if (isDaily) {
+    return httpGet(
+      BASE_URL + dailiesExt + id + parameters,
+      fileNameForCache: BASE_URL + id + '/' + ext,
+      skipCache: skipCache,
+    );
+  } else {
+    return httpGet(
+      BASE_URL + ext + id + parameters,
+      fileNameForCache: BASE_URL + id + '/' + ext,
+      skipCache: skipCache,
+    );
+  }
+}
