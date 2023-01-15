fc4b7e8086128d0faada45715b4c5db018b12f5d
Michael Speed <michaelcspeed@gmail.com>
2023-01-15 20:14:47 +0100

    Fix build

--- android/app/src/main/java/meditofoundation/medito/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package meditofoundation.medito;
-
-import io.flutter.embedding.android.FlutterActivity;
-public class MainActivity extends FlutterActivity {
-
-}
--- android/app/src/main/kotlin/meditofoundation/meditoapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package meditofoundation.meditoapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
