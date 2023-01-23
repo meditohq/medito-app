f335c732d9d243fcaccb7f83d6fda27c11f1c9e4
Michael Speed <michaelcspeed@gmail.com>
2023-01-23 19:45:00 +0100

    removed unused code

--- lib/audioplayer/screen_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:audio_service/audio_service.dart';
-
-class ScreenState {
-  final List<MediaItem> queue;
-  final MediaItem mediaItem;
-  final PlaybackState playbackState;
-
-  ScreenState(this.queue, this.mediaItem, this.playbackState);
-}
