d26e6c0af5277684106b8583777b258850ef39f2
Michael Speed <michaelcspeed@gmail.com>
2023-02-22 19:48:40 +0100

    Fix .env path

--- .github/scripts/create_auth_dart.sh
+++ .github/scripts/create_auth_dart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Fixme in https://github.com/meditohq/medito-app/issues/151
-echo "const BASE_URL = '$BASE_URL';" >> lib/.env && \
-echo "***REMOVED*** '$SENTRY_URL';" >> lib/.env && \
-echo "***REMOVED*** '$CONTENT_TOKEN';" >> lib/.env && \
-echo "***REMOVED*** '$INIT_TOKEN';" >> lib/.env
+echo "const BASE_URL = '$BASE_URL';" >> .env && \
+echo "***REMOVED*** '$SENTRY_URL';" >> .env && \
+echo "***REMOVED*** '$CONTENT_TOKEN';" >> .env && \
+echo "***REMOVED*** '$INIT_TOKEN';" >> .env
