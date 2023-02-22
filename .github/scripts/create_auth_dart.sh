51361183fc0278bf81bbc52430aab827ec085744
Michael Speed <michaelcspeed@gmail.com>
2023-02-22 19:32:04 +0100

    Update create_auth_dart.sh

--- .github/scripts/create_auth_dart.sh
+++ .github/scripts/create_auth_dart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Fixme in https://github.com/meditohq/medito-app/issues/151
-echo "const BASE_URL = '$BASE_URL';" >> lib/network/auth.dart && \
-echo "***REMOVED*** '$SENTRY_URL';" >> lib/network/auth.dart && \
-echo "***REMOVED*** '$CONTENT_TOKEN';" >> lib/network/auth.dart && \
-echo "***REMOVED*** '$INIT_TOKEN';" >> lib/network/auth.dart
+echo "const BASE_URL = '$BASE_URL';" >> lib/.env && \
+echo "***REMOVED*** '$SENTRY_URL';" >> lib/.env && \
+echo "***REMOVED*** '$CONTENT_TOKEN';" >> lib/.env && \
+echo "***REMOVED*** '$INIT_TOKEN';" >> lib/.env
