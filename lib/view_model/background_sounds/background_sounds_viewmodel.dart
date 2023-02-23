f5ad2bff46ea78a82e2bda0df98c429c1cb39d1a
Osama Asif <osama.asif20@gmail.com>
2023-02-23 16:37:32 +0500

    ui: background screen ui completed

--- lib/view_model/background_sounds/background_sounds_viewmodel.dart
+++ lib/view_model/background_sounds/background_sounds_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:Medito/models/models.dart';
 import 'package:Medito/repositories/repositories.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'background_sounds_viewmodel.g.dart';
 
@@ -8,4 +10,24 @@ Future<List<BackgroundSoundsModel>> backgroundSounds(BackgroundSoundsRef ref) {
   final backgroundSoundsRepository =
       ref.watch(backgroundSoundsRepositoryProvider);
   return backgroundSoundsRepository.fetchBackgroundSounds();
+}
+
+final backgroundSoundsNotifierProvider =
+    ChangeNotifierProvider<BackgroundSoundsNotifier>((ref) {
+  return BackgroundSoundsNotifier();
+});
+
+class BackgroundSoundsNotifier extends ChangeNotifier {
+  double volume = 0;
+  BackgroundSoundsModel? selectedBgSound;
+
+  void handleOnChangeVolume(double _volume) {
+    volume = _volume;
+    notifyListeners();
+  }
+
+  void handleOnChangeSound(BackgroundSoundsModel _sound) {
+    selectedBgSound = _sound;
+    notifyListeners();
+  }
 }
---
+++ lib/views/background_sound/components/volume_slider_component.dart
@@ -0,0 +1,38 @@
+import 'package:Medito/constants/constants.dart';
+import 'package:Medito/view_model/background_sounds/background_sounds_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'custom_track_shape_component.dart';
+
+class VolumeSliderComponent extends ConsumerWidget {
+  const VolumeSliderComponent({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bgSoundNotifierProvider = ref.watch(backgroundSoundsNotifierProvider);
+    var currentVolume = bgSoundNotifierProvider.volume;
+    return SliderTheme(
+      data: SliderThemeData(
+          trackShape: CustomTrackShapeComponent(
+              leadingTitle: 'Volume',
+              tralingText: currentVolume.toString().split('.').first + '%'),
+          overlayShape: RoundSliderOverlayShape(overlayRadius: 0.0),
+          thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0.0),
+          trackHeight: 72),
+      child: Slider(
+        value: currentVolume,
+        min: 0,
+        max: 100,
+        divisions: 100,
+        activeColor: ColorConstants.purple,
+        inactiveColor: ColorConstants.greyIsTheNewGrey,
+        onChanged: (double newValue) {
+          bgSoundNotifierProvider.handleOnChangeVolume(newValue);
+        },
+        semanticFormatterCallback: (double newValue) {
+          return '${newValue.round()} ';
+        },
+      ),
+    );
+  }
+}
