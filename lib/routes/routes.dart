1f5fcc8f4e35b6fdf062e1361ddb9581b4fd1005
Osama Asif <osama.asif20@gmail.com>
2023-02-27 14:47:23 +0500

    fix: player screen route fixed

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -10,8 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../views/home/home_wrapper_widget.dart';
-// import '../views/player_old/player2/player_view.dart';
-import '../views/player/player_view.dart';
+import '../views/player_old/player2/player_view.dart';
+// import '../views/player/player_view.dart';
 
 const String SessionPath = '/session/:sid';
 const String DailyPath = '/daily/:did';
@@ -178,12 +178,11 @@ MaterialPage<void> getCollectionMaterialPage(GoRouterState state) {
 
 MaterialPage<void> getPlayerMaterialPage(GoRouterState state) {
   var session = state.extra as SessionModel;
-  return MaterialPage(
-    key: state.pageKey,
-    child: PlayerView(
-      sessionModel: session,
-    ),
-  );
+  return MaterialPage(key: state.pageKey, child: PlayerWidget()
+      // child: PlayerView(
+      //   sessionModel: session,
+      // ),
+      );
 }
 
 MaterialPage<void> getFolderMaterialPage(GoRouterState state) {
