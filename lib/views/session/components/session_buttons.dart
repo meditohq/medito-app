caa60604f1d1d63e6e3873331313439e4df70b9e
Osama Asif <osama.asif20@gmail.com>
2023-03-08 14:54:54 +0500

    chore: commented code removed

--- lib/views/session/components/session_buttons.dart
+++ lib/views/session/components/session_buttons.dart
@@ -1,11 +1,7 @@
-import 'package:Medito/audioplayer/audio_inherited_widget.dart';
-import 'package:Medito/audioplayer/media_lib.dart';
 import 'package:Medito/constants/constants.dart';
 import 'package:Medito/models/models.dart';
-import 'package:Medito/services/network/dio_client_provider.dart';
 import 'package:Medito/routes/routes.dart';
 import 'package:Medito/utils/utils.dart';
-import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -61,32 +57,6 @@ class SessionButtons extends StatelessWidget {
     return Consumer(
       builder: (context, ref, child) => InkWell(
         onTap: () async {
-          try {
-            var fileUrl =
-                'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3';
-            AudioHandler? _audioHandler =
-                AudioHandlerInheritedWidget.of(context).audioHandler;
-            var mediaItem = MediaItem(
-                //TODO
-                id: fileUrl,
-                // id: file.path.toString(),
-                title: title,
-                duration: Duration(milliseconds: file.duration),
-                artUri: Uri.parse(coverUrl),
-                extras: {
-                  HAS_BG_SOUND: true,
-                  LOCATION: null,
-                  DURATION: file.duration
-                });
-            await _audioHandler.playMediaItem(mediaItem);
-          } catch (e) {
-            createSnackBar(
-              e.toString(),
-              context,
-            );
-            print(e);
-          }
-
           context.go(
             GoRouter.of(context).location + PlayerPath,
             extra: {'sessionModel': sessionModel, 'file': file},
