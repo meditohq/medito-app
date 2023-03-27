9f3a3b47041fa713819935fdf34705c965888943
Michael Speed <michaelcspeed@gmail.com>
2023-03-27 20:19:32 +0200

    Added auth.dart to fix pipeline

--- lib/network/auth.dart
+++ lib/network/auth.dart
@@ -1,8 +1,4 @@
-@Deprecated('Remove after refactor')
 const BASE_URL = '';
-@Deprecated('Remove after refactor')
 const INIT_TOKEN = '';
-@Deprecated('Remove after refactor')
 const CONTENT_TOKEN = '';
-@Deprecated('Remove after refactor')
 const SENTRY_URL = '';
