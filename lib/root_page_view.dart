37143617070fe41900e4f73e70cd3a0a7611b791
Osama Asif <osama.asif20@gmail.com>
2023-04-15 17:39:54 +0500

    fix: shell route implemented for root page view

--- lib/root_page_view.dart
+++ lib/root_page_view.dart
@@ -1,14 +1,9 @@
 import 'package:Medito/constants/constants.dart';
-import 'package:Medito/models/session/session_model.dart';
-import 'package:Medito/utils/utils.dart';
-import 'package:Medito/view_model/background_sounds/background_sounds_viewmodel.dart';
 import 'package:Medito/view_model/page_view/page_view_viewmodel.dart';
-import 'package:Medito/view_model/player/download/audio_downloader_viewmodel.dart';
 import 'package:Medito/view_model/player/player_viewmodel.dart';
 import 'package:Medito/views/player/player_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'view_model/audio_player/audio_player_viewmodel.dart';
 import 'views/player/components/mini_player_widget.dart';
 
 class RootPageView extends ConsumerStatefulWidget {
@@ -22,7 +17,6 @@ class RootPageView extends ConsumerStatefulWidget {
 class _RootPageViewtState extends ConsumerState<RootPageView> {
   @override
   void initState() {
-    ref.read(playerProvider.notifier).getCurrentlyPlayingSession();
     ref.read(pageviewNotifierProvider).addListenerToPage();
     super.initState();
   }
@@ -30,14 +24,6 @@ class _RootPageViewtState extends ConsumerState<RootPageView> {
   @override
   Widget build(BuildContext context) {
     final currentlyPlayingSession = ref.watch(playerProvider);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (currentlyPlayingSession != null) {
-        checkAudioLocally(
-          currentlyPlayingSession,
-          currentlyPlayingSession.audio.first.files.first,
-        );
-      }
-    });
     var radius = Radius.circular(currentlyPlayingSession != null ? 15 : 0);
 
     return Scaffold(
@@ -45,7 +31,6 @@ class _RootPageViewtState extends ConsumerState<RootPageView> {
       body: PageView(
         controller: ref.read(pageviewNotifierProvider).pageController,
         scrollDirection: Axis.vertical,
-        // physics: BouncingScrollPhysics(),
         children: [
           Column(
             children: [
@@ -92,60 +77,5 @@ class _RootPageViewtState extends ConsumerState<RootPageView> {
         ],
       ),
     );
-  }
-
-  void checkAudioLocally(SessionModel sessionModel, SessionFilesModel file) {
-    loadSessionAndBackgroundSound(sessionModel, file);
-    // ref.read(audioPlayPauseStateProvider.notifier).state =
-    //     PLAY_PAUSE_AUDIO.PLAY;
-  }
-
-  void loadSessionAndBackgroundSound(
-    SessionModel sessionModel,
-    SessionFilesModel file,
-  ) {
-    final _audioPlayerNotifier = ref.read(audioPlayerNotifierProvider);
-    var isPlaying = _audioPlayerNotifier.sessionAudioPlayer.playerState.playing;
-    var _currentPlayingFileId =
-        _audioPlayerNotifier.currentlyPlayingSession?.id;
-
-    if (!isPlaying || _currentPlayingFileId != file.id) {
-      setSessionAudio(_audioPlayerNotifier, sessionModel, file);
-      setBackgroundSound(_audioPlayerNotifier, sessionModel.hasBackgroundSound);
-    }
-  }
-
-  void setSessionAudio(
-    AudioPlayerNotifier _audioPlayerNotifier,
-    SessionModel sessionModel,
-    SessionFilesModel file,
-  ) {
-    var checkDownloadedFile = ref.read(audioDownloaderProvider).getSessionAudio(
-          '${sessionModel.id}-${file.id}${getFileExtension(file.path)}',
-        );
-    checkDownloadedFile.then((value) {
-      _audioPlayerNotifier.setSessionAudio(file, filePath: value);
-      _audioPlayerNotifier.currentlyPlayingSession = file;
-    });
-  }
-
-  void setBackgroundSound(
-    AudioPlayerNotifier _audioPlayerNotifier,
-    bool hasBackgroundSound,
-  ) {
-    if (hasBackgroundSound) {
-      final _provider = ref.read(backgroundSoundsNotifierProvider);
-      _provider.getBackgroundSoundFromPref().then((_) {
-        if (_provider.selectedBgSound != null &&
-            _provider.selectedBgSound?.title != StringConstants.NONE) {
-          _audioPlayerNotifier.setBackgroundAudio(_provider.selectedBgSound!);
-        }
-      });
-      _provider.getVolumeFromPref().then((_) {
-        _audioPlayerNotifier.setBackgroundSoundVolume(_provider.volume);
-      });
-    }else{
-      _audioPlayerNotifier.pauseBackgroundSound();
-    }
   }
 }
