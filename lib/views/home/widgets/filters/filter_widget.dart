4faf7d96bb70f8aa01ec1898826c9c748d2ab926
Osama Asif <osama.asif20@gmail.com>
2023-05-03 16:52:26 +0500

    ui: filters list added

--- lib/views/home/widgets/filters/filter_widget.dart
+++ lib/views/home/widgets/filters/filter_widget.dart
@@ -3,53 +3,68 @@ import 'package:Medito/constants/strings/asset_constants.dart';
 import 'package:Medito/constants/styles/widget_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:collection/collection.dart';
 
 class FilterWidget extends StatelessWidget {
   const FilterWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var items = [
+      '💤 Sleep',
+      '🚨 Emergency',
+      '❤️ Relationships',
+      '☁︎ Breathing',
+      '☀️ Daily',
+      '⏳ Timer',
+      '📚 Courses',
+      '🏆 Challenges',
+    ];
+
+    var row1 = items.take(items.length ~/ 2).toList();
+    var row2 = items.skip(items.length ~/ 2).toList();
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _filterListView(),
-            _filterListView(),
+            _filterListView(row1),
+            _filterListView(row2),
           ],
         ),
       ),
     );
   }
 
-  SizedBox _filterListView() {
-    return SizedBox(
-      height: 40,
-      child: ListView.builder(
-        itemCount: 14,
-        scrollDirection: Axis.horizontal,
-        physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        itemBuilder: (context, index) => Container(
-          // height: 40,
-          decoration: BoxDecoration(
-            color: ColorConstants.onyx,
-            borderRadius: BorderRadius.circular(16),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SvgPicture.asset(
-                AssetConstants.icStreak,
+  Padding _filterListView(List<String> items) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8),
+      child: SizedBox(
+        height: 50,
+        child: ListView.builder(
+          itemCount: items.length,
+          scrollDirection: Axis.horizontal,
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemBuilder: (context, index) => Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: ColorConstants.onyx,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding:
+                    EdgeInsets.only(left: 15, right: 15, bottom: 10, top: 6),
+                child: Text(
+                  items[index],
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
               ),
-              width4,
-              Text(
-                'data',
-                style: Theme.of(context).textTheme.titleSmall,
-              ),
-            ],
+            ),
           ),
         ),
       ),
