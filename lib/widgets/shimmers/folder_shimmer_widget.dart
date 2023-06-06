46af74fe7fbba7f876207975c0b635f5567f82e8
Osama Asif <osama.asif20@gmail.com>
2023-06-06 19:06:37 +0500

    Merge branch 'develop' of https://github.com/meditohq/medito-app into rename-components-to-widget

--- lib/widgets/shimmers/folder_shimmer_widget.dart
+++ lib/widgets/shimmers/folder_shimmer_widget.dart
@@ -8,7 +8,7 @@ class FolderShimmerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
+    
     return SingleChildScrollView(
       child: Column(
         children: [
