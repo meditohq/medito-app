1d5ee4bd380387b0bfcbcdfe22bfbb6bca4e8f8f
Osama Asif <osama.asif20@gmail.com>
2023-07-27 02:35:10 +0500

    chore: menu bottom sheet fixed

--- lib/constants/styles/widget_styles.dart
+++ lib/constants/styles/widget_styles.dart
@@ -1,3 +1,4 @@
+import 'package:Medito/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,3 +12,10 @@ const height8 = SizedBox(height: 8);
 const width8 = SizedBox(width: 8);
 const height4 = SizedBox(height: 4);
 const width4 = SizedBox(width: 4);
+const bottomSheetBoxDecoration = BoxDecoration(
+  color: ColorConstants.onyx,
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(12),
+    topRight: Radius.circular(12),
+  ),
+);
