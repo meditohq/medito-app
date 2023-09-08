cfa4856278b97a924c282fd9c6f711a7a8f7cfae
Osama Asif <osama.asif20@gmail.com>
2023-09-09 03:24:30 +0500

    fix: home screen bg color fixed

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -10,11 +10,43 @@ import 'widgets/search/search_widget.dart';
 import 'widgets/meditation_cards/card_list_widget.dart';
 import 'package:Medito/providers/providers.dart';
 
-class HomeView extends ConsumerWidget {
-  const HomeView({super.key});
+class HomeView extends ConsumerStatefulWidget {
+  HomeView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  final ScrollController _scrollController = ScrollController();
+  Color _backgroundColor = ColorConstants.ebony;
+  var _announcementColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_announcementColor != null) {
+      final newColor = _scrollController.offset > 100
+          ? ColorConstants.ebony
+          : _announcementColor;
+      setState(() {
+        _backgroundColor = newColor;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     var homeRes = ref.watch(homeProvider);
     var stats = ref.watch(remoteStatsProvider);
     final currentlyPlayingSession = ref.watch(playerProvider);
@@ -23,63 +55,74 @@ class HomeView extends ConsumerWidget {
       body: homeRes.when(
         skipLoadingOnRefresh: true,
         skipLoadingOnReload: true,
-        data: (data) => SafeArea(
-          top: data.announcement == null,
-          bottom: false,
-          child: Container(
-            color: data.announcement?.colorBackground != null
-                ? ColorConstants.getColorFromString(
-                    data.announcement!.colorBackground,
-                  )
-                : null,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+        data: (data) {
+          var hasAnnouncement = data.announcement == null;
+          if (!hasAnnouncement) {
+            _announcementColor = ColorConstants.getColorFromString(
+              data.announcement!.colorBackground,
+            );
+          }
+
+          return SafeArea(
+            top: hasAnnouncement,
+            bottom: false,
+            child: Stack(
               children: [
-                Expanded(
-                  child: RefreshIndicator(
-                    onRefresh: () async {
-                      ref.invalidate(homeProvider);
-                      await ref.read(homeProvider.future);
-                      ref.invalidate(remoteStatsProvider);
-                      await ref.read(remoteStatsProvider.future);
-                    },
-                    child: SingleChildScrollView(
-                      physics: BouncingScrollPhysics(),
-                      child: Container(
-                        color: ColorConstants.ebony,
-                        child: Column(
-                          children: [
-                            _getAnnouncementBanner(data),
-                            HomeHeaderWidget(
-                              homeMenuModel: data.menu,
-                              miniStatsModel: stats.asData?.value.mini,
+                Container(
+                  color: _backgroundColor,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: RefreshIndicator(
+                          onRefresh: () async {
+                            ref.invalidate(homeProvider);
+                            await ref.read(homeProvider.future);
+                            ref.invalidate(remoteStatsProvider);
+                            await ref.read(remoteStatsProvider.future);
+                          },
+                          child: SingleChildScrollView(
+                            controller: _scrollController,
+                            physics: BouncingScrollPhysics(),
+                            child: Container(
+                              color: ColorConstants.ebony,
+                              child: Column(
+                                children: [
+                                  _getAnnouncementBanner(data),
+                                  HomeHeaderWidget(
+                                    homeMenuModel: data.menu,
+                                    miniStatsModel: stats.asData?.value.mini,
+                                  ),
+                                  Column(
+                                    children: [
+                                      SearchWidget(),
+                                      height8,
+                                      FilterWidget(
+                                        chips: data.chips,
+                                      ),
+                                      height16,
+                                      height16,
+                                      _cardListWidget(data),
+                                      SizedBox(
+                                        height: currentlyPlayingSession != null
+                                            ? 16
+                                            : 48,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                            Column(
-                              children: [
-                                SearchWidget(),
-                                height8,
-                                FilterWidget(
-                                  chips: data.chips,
-                                ),
-                                height16,
-                                height16,
-                                _cardListWidget(data),
-                                SizedBox(
-                                  height:
-                                      currentlyPlayingSession != null ? 16 : 48,
-                                ),
-                              ],
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ),
+          );
+        },
         error: (err, stack) => MeditoErrorWidget(
           message: err.toString(),
           onTap: () => ref.refresh(homeProvider),
