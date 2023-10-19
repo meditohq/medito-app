7538a124c8e7eb9ed3c0c991ff489d029f73bc70
romain <scouech+github@pm.me>
2023-10-19 17:10:50 +0200

    update webview widget

--- lib/widgets/medito_webview_widget.dart
+++ lib/widgets/medito_webview_widget.dart
@@ -3,6 +3,7 @@ import 'package:Medito/views/main/app_bar_widget.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -10,6 +11,7 @@ class MeditoWebViewWidget extends StatefulWidget {
   final String url;
 
   const MeditoWebViewWidget({Key? key, required this.url}) : super(key: key);
+
   @override
   _MeditoWebViewWidgetState createState() => _MeditoWebViewWidgetState();
 }
@@ -18,13 +20,12 @@ class _MeditoWebViewWidgetState extends State<MeditoWebViewWidget> {
   var controller = WebViewController();
   int _stackToView = 1;
 
-
   @override
   void initState() {
     try {
       controller
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setBackgroundColor(const Color(0x00000000))
+        ..setBackgroundColor(ColorConstants.ebony)
         ..setNavigationDelegate(
           NavigationDelegate(
             onPageFinished: _handleLoad,
@@ -57,7 +58,7 @@ class _MeditoWebViewWidgetState extends State<MeditoWebViewWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorConstants.greyIsTheNewGrey,
+      backgroundColor: ColorConstants.onyx,
       appBar: MeditoAppBarWidget(
         hasCloseButton: true,
       ),
@@ -66,7 +67,7 @@ class _MeditoWebViewWidgetState extends State<MeditoWebViewWidget> {
         children: [
           WebViewWidget(
             controller: controller,
-            gestureRecognizers: Set()
+            gestureRecognizers: {}
               ..add(Factory(() => EagerGestureRecognizer())),
           ),
           const Center(
