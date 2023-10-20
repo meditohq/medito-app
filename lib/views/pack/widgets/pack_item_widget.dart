7e4037661e934440204355fd5a69f2e5acd6f0bf
romain <scouech+github@pm.me>
2023-10-20 21:23:45 +0200

    Change vertical padding on pack list item

--- lib/views/pack/widgets/pack_item_widget.dart
+++ lib/views/pack/widgets/pack_item_widget.dart
@@ -22,7 +22,7 @@ class PackItemWidget extends StatelessWidget {
                 bottom: BorderSide(width: 2, color: ColorConstants.charcoal),
               ),
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
