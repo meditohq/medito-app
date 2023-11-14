95d5f2d980bd5b02e2687a68eb9f39d3865b44fb
romain <scouech+github@pm.me>
2023-11-14 21:23:00 +0100

    Change purple color

--- lib/constants/colors/color_constants.dart
+++ lib/constants/colors/color_constants.dart
@@ -25,7 +25,7 @@ class ColorConstants {
   static const walterWhite = Colors.white;
   static const softGrey = Color(0xff424345);
   static const quiteGrey = Color(0xffD5D3D1);
-  static const lightPurple = Color(0xff917DF0);
+  static const lightPurple = Color(0xff9D8CF2);
   static const onyx = Color(0xff2A2A32);
   static const ebony = Color(0xff171718);
   static const graphite = Color(0xffAAAAAA);
