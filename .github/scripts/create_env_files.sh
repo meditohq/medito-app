2c93bb81ee50748a89da4beaf52a5345aaef2c89
Michael Speed <michaelcspeed@gmail.com>
2023-11-16 21:02:52 +0100

    Update dependencies

--- .github/scripts/create_env_files.sh
+++ .github/scripts/create_env_files.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Fixme in https://github.com/meditohq/medito-app/issues/151
 echo "const BASE_URL = '$BASE_URL';" >> /.prod.env && \
 echo "const SENTRY_URL = '$SENTRY_URL';" >> /.prod.env && \
 echo "const INIT_TOKEN = '$INIT_TOKEN';" >> /.prod.env
+echo "const BASE_URL = '$BASE_URL';" >> /.staging.env && \
+echo "const SENTRY_URL = '$SENTRY_URL';" >> /.staging.env && \
+echo "const INIT_TOKEN = '$INIT_TOKEN';" >> /.staging.env
