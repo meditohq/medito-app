60604873aeff24858d609672826f20614cff3fe6
Michael Speed <michaelcspeed@gmail.com>
2023-11-16 20:31:42 +0100

    Attempt fixing pipeline

--- .github/scripts/create_auth_dart.sh
+++ .github/scripts/create_auth_dart.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Fixme in https://github.com/meditohq/medito-app/issues/151
-echo "const BASE_URL = '$BASE_URL';" >> .env && \
-echo "const SENTRY_URL = '$SENTRY_URL';" >> .env && \
-echo "const CONTENT_TOKEN = '$CONTENT_TOKEN';" >> .env && \
-echo "const INIT_TOKEN = '$INIT_TOKEN';" >> .env
+echo "const BASE_URL = '$BASE_URL';" >> /.prod.env && \
+echo "const SENTRY_URL = '$SENTRY_URL';" >> /.prod.env && \
+echo "const INIT_TOKEN = '$INIT_TOKEN';" >> /.prod.env
