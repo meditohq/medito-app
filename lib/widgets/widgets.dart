048556b2782375548e7a0109187b013d6f33e782
romain <scouech+github@pm.me>
2023-11-22 15:39:33 +0100

    Remove custom close button

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -5,7 +5,6 @@ export 'shimmers/background_sounds_shimmer_widget.dart';
 export 'errors/medito_error_widget.dart';
 export 'errors/connectivity_error_widget.dart';
 export 'headers/medito_app_bar_large.dart';
-export 'buttons/close_button_widget.dart';
 export 'buttons/loading_button_widget.dart';
 export 'medito_webview_widget.dart';
 export 'markdown_widget.dart';
