98853124f516942a0ff27d186391a6c6a3957139
Michael Speed <michaelcspeed@gmail.com>
2024-01-23 18:18:41 +0100

    Remove unused files

--- lib/providers/player/audio_state.dart
+++
@@ -1,6 +0,0 @@
-class AudioState {
-  final bool isPlaying;
-  final Duration position;
-
-  AudioState({required this.isPlaying, required this.position});
-}
--- lib/providers/player/audio_state_constants.dart
+++
@@ -1,6 +0,0 @@
-class AudioStateConstants {
-  static const String channelName = 'com.example.app/audio';
-  static const String audioStateUpdateMethod = 'audioStateUpdate';
-  static const String isPlayingKey = 'isPlaying';
-  static const String position = 'position';
-}
