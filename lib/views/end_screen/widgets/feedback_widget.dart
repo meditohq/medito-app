8a8d0b968bc2bd7462a619ab1ca0fb4274bf47bc
Michael Speed <michaelcspeed@gmail.com>
2024-02-04 13:38:24 +0100

    Fix feedback screen

--- lib/views/end_screen/widgets/feedback_widget.dart
+++ lib/views/end_screen/widgets/feedback_widget.dart
@@ -1,7 +1,10 @@
 import 'package:Medito/constants/constants.dart';
 import 'package:Medito/models/models.dart';
+import 'package:Medito/providers/player/player_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../providers/events/events_provider.dart';
 
 class FeedbackWidget extends ConsumerStatefulWidget {
   const FeedbackWidget({super.key, required this.feedbackModel});
@@ -17,33 +20,34 @@ class _FeedbackWidgetState extends ConsumerState<FeedbackWidget> {
   bool isFeedbackAdded = false;
 
   void _handleFeedbackPress(String feedback) async {
-    // final audioProvider = ref.read(audioPlayerNotifierProvider);
-    // var extras = audioProvider.mediaItem.value?.extras;
+    final audioProvider = ref.read(playerProvider);
+    var trackId = audioProvider?.id ?? '';
+    var audioFileId = audioProvider?.audio.first.files.first.id ?? '';
 
     setState(() {
       isLoading = true;
     });
-    // var payload = FeedbackTappedModel(
-    //   trackId: extras?[TypeConstants.trackIdKey] ?? '',
-    //   audioFileId: extras?[TypeConstants.fileIdKey] ?? '',
-    //   emoji: feedback,
-    // );
-    // var event = EventsModel(
-    //   name: EventTypes.trackFeedback,
-    //   payload: payload.toJson(),
-    // );
-    // try {
-    //   await ref.read(eventsProvider(event: event.toJson()).future);
-    //   setState(() {
-    //     isLoading = false;
-    //     isFeedbackAdded = true;
-    //   });
-    // } catch (e) {
-    //   setState(() {
-    //     isLoading = false;
-    //     isFeedbackAdded = false;
-    //   });
-    // }
+    var payload = FeedbackTappedModel(
+      trackId: trackId,
+      audioFileId: audioFileId,
+      emoji: feedback,
+    );
+    var event = EventsModel(
+      name: EventTypes.trackFeedback,
+      payload: payload.toJson(),
+    );
+    try {
+      await ref.read(eventsProvider(event: event.toJson()).future);
+      setState(() {
+        isLoading = false;
+        isFeedbackAdded = true;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+        isFeedbackAdded = false;
+      });
+    }
   }
 
   @override
