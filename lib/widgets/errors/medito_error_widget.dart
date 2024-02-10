5a3860f9f771fd2bd9aa2a183fa4dd75398cd921
Michael Speed <michaelcspeed@gmail.com>
2024-02-10 11:57:10 +0100

    Remove todo

--- lib/widgets/errors/medito_error_widget.dart
+++ lib/widgets/errors/medito_error_widget.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-//todo: fix shouldShowCheckDownloadButton
 class MeditoErrorWidget extends ConsumerWidget {
   const MeditoErrorWidget({
     Key? key,
