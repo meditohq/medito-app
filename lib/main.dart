dc09569346617d1c25e5fef73efb2f0742c7a303
Michael Speed <michaelcspeed@gmail.com>
2024-03-26 15:59:49 +0100

    Fix env var

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ import 'constants/environments/environment_constants.dart';
 import 'services/notifications/notifications_service.dart';
 
 var audioStateNotifier = AudioStateNotifier();
-var currentEnvironment = EnvironmentConstants.stagingEnv;
+var currentEnvironment = kReleaseMode ? EnvironmentConstants.prodEnv : EnvironmentConstants.stagingEnv;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
