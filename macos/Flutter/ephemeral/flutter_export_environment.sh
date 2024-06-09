5c227851bffe139f1cb23d8db1657eb630c1c19a
Michael Speed <michaelcspeed@gmail.com>
2024-06-09 19:09:06 +0200

    update

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mike/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mike/Projects/meditoapp"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=3.0.33"
+export "FLUTTER_BUILD_NUMBER=30033"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
