c0ae0da1e60ea47ce94a34c775385b0c09f720d7
Michael Speed <michaelcspeed@gmail.com>
2024-07-23 19:28:24 +0200

    Animate icon

--- lib/views/home/widgets/stats/stats_row.dart
+++ lib/views/home/widgets/stats/stats_row.dart
@@ -1,8 +1,10 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 import '../../../../constants/colors/color_constants.dart';
 
-class StatsRow extends StatelessWidget {
+class StatsRow extends StatefulWidget {
   final VoidCallback leftButtonClicked;
   final VoidCallback rightButtonClicked;
   final String leftButtonText;
@@ -25,19 +27,69 @@ class StatsRow extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _StatsRowState createState() => _StatsRowState();
+}
+
+class _StatsRowState extends State<StatsRow> with TickerProviderStateMixin {
+  late AnimationController _pulseController;
+  late AnimationController _rotationController;
+  late AnimationController _fadeController;
+  late Animation<double> _pulseAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _pulseController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _rotationController = AnimationController(
+      duration: const Duration(seconds: 500),
+      vsync: this,
+    )..repeat();
+
+    _fadeController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _pulseAnimation = CurvedAnimation(
+      parent: _pulseController,
+      curve: Curves.easeInOut,
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.9, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _fadeController,
+        curve: Curves.easeInOutExpo,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pulseController.dispose();
+    _rotationController.dispose();
+    _fadeController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final buttonShape = RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(7.0),
     );
 
     var leftButtonStyle = TextButton.styleFrom(
-      iconColor: leftButtonIconColor,
+      iconColor: widget.leftButtonIconColor,
       backgroundColor: ColorConstants.amsterdamSummer,
       shape: buttonShape,
     );
 
     var rightButtonStyle = TextButton.styleFrom(
-      iconColor: rightButtonIconColor,
+      iconColor: widget.rightButtonIconColor,
       backgroundColor: ColorConstants.amsterdamSummer,
       shape: buttonShape,
     );
@@ -50,18 +102,18 @@ class StatsRow extends StatelessWidget {
           Expanded(
             flex: 1,
             child: TextButton(
-              onPressed: leftButtonClicked,
+              onPressed: widget.leftButtonClicked,
               style: leftButtonStyle,
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Icon(
-                    leftButtonIcon,
+                    widget.leftButtonIcon,
                     size: 20,
                   ),
                   SizedBox(width: 4),
                   Text(
-                    leftButtonText,
+                    widget.leftButtonText,
                     style: TextStyle(
                       color: ColorConstants.walterWhite,
                       fontSize: 14,
@@ -75,18 +127,35 @@ class StatsRow extends StatelessWidget {
           Expanded(
             flex: 1,
             child: TextButton(
-              onPressed: rightButtonClicked,
+              onPressed: widget.rightButtonClicked,
               style: rightButtonStyle,
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Icon(
-                    rightButtonIcon,
-                    size: 20,
+                  AnimatedBuilder(
+                    animation: Listenable.merge(
+                      [_pulseAnimation, _rotationController, _fadeAnimation],
+                    ),
+                    builder: (context, child) {
+                      return Transform.rotate(
+                        angle: _rotationController.value * 2 * math.pi,
+                        child: Opacity(
+                          opacity: _fadeAnimation.value,
+                          child: Transform.scale(
+                            scale: 1.0 + (_pulseAnimation.value * 0.1),
+                            child: Icon(
+                              widget.rightButtonIcon,
+                              size: 20,
+                              color: widget.rightButtonIconColor,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                   SizedBox(width: 4),
                   Text(
-                    rightButtonText,
+                    widget.rightButtonText,
                     style: TextStyle(
                       color: ColorConstants.walterWhite,
                       fontSize: 14,
