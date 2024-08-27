2f46d685eb0077fab9e07c4a3d87774d0ab29521
Michael Speed <michaelcspeed@gmail.com>
2024-08-28 00:30:18 +0200

    Explore landscape

--- lib/views/explore/widgets/explore_initial_page_widget.dart
+++ lib/views/explore/widgets/explore_initial_page_widget.dart
@@ -26,47 +26,99 @@ class ExploreInitialPageWidget extends ConsumerWidget {
     );
   }
 
-  Column _buildMain(
-    WidgetRef ref,
-    List<PackItemsModel> packs,
-    BuildContext context,
-  ) {
-    return Column(
-      children: [
-        Expanded(
-          child: RefreshIndicator(
-            onRefresh: () async => ref.refresh(fetchAllPacksProvider),
-            child: ListView.builder(
-              itemCount: packs.length,
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).padding.top + padding16,
-                left: padding16,
-                right: padding16,
-                bottom: padding16,
+  Widget _buildMain(
+      WidgetRef ref,
+      List<PackItemsModel> packs,
+      BuildContext context,
+      ) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        final isPortrait = orientation == Orientation.portrait;
+        return Column(
+          children: [
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: () async => ref.refresh(fetchAllPacksProvider),
+                child: isPortrait
+                    ? _buildListView(context, packs, ref)
+                    : _buildGridView(context, packs, ref),
               ),
-              itemBuilder: (context, index) {
-                var element = packs[index];
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: padding16),
-                  child: PackCardWidget(
-                    title: element.title,
-                    subTitle: element.subtitle,
-                    coverUrlPath: element.coverUrl,
-                    onTap: () => handleNavigation(
-                      element.type,
-                      [element.id.toString(), element.path],
-                      context,
-                      ref: ref,
-                    ),
-                  ),
-                );
-              },
+  Widget _buildListView(
+      BuildContext context,
+      List<PackItemsModel> packs,
+      WidgetRef ref,
+      ) {
+    return ListView.builder(
+      itemCount: packs.length,
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top + padding16,
+        left: padding16,
+        right: padding16,
+        bottom: padding16,
+      ),
+      itemBuilder: (context, index) {
+        var element = packs[index];
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: padding16),
+          child: PackCardWidget(
+            title: element.title,
+            subTitle: element.subtitle,
+            coverUrlPath: element.coverUrl,
+            onTap: () => handleNavigation(
+              element.type,
+              [element.id.toString(), element.path],
+              context,
+              ref: ref,
             ),
           ),
-        ),
-      ],
+        );
+      },
+    );
+  }
+
+  Widget _buildGridView(
+      BuildContext context,
+      List<PackItemsModel> packs,
+      WidgetRef ref,
+      ) {
+    return GridView.builder(
+      itemCount: packs.length,
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top + padding16,
+        left: padding16,
+        right: padding16,
+        bottom: padding16,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: padding16,
+        mainAxisSpacing: padding16,
+        childAspectRatio: 8 / 2,
+      ),
+      itemBuilder: (context, index) {
+        var element = packs[index];
+
+        return PackCardWidget(
+          title: element.title,
+          subTitle: element.subtitle,
+          coverUrlPath: element.coverUrl,
+          onTap: () => handleNavigation(
+            element.type,
+            [element.id.toString(), element.path],
+            context,
+            ref: ref,
+          ),
+        );
+      },
     );
   }
 
