4977f24a8c4248b0f46bcd8e724c03fe3dfed46a
Luz Ams <luzracca@gmail.com>
2024-10-30 16:43:53 +0100

    Removing Kind == Large (not used yet)

--- ios/MeditoWidget/View/MeditoWidgetKind.swift
+++ ios/MeditoWidget/View/MeditoWidgetKind.swift
@@ -3,5 +3,4 @@ import Foundation
 enum MeditoWidgetKind {
     static let streakSmall = "StreakWidgetSmall"
     static let streakMedium = "StreakWidgetMedium"
-    static let streakLarge = "StreakWidgetLarge"
 }
