a391255c9643d039f337b0e07f43296a43a4b091
Michael Speed <michaelcspeed@gmail.com>
2024-10-31 19:07:44 +0100

    Refresh widget in background

---
+++ lib/constants/work_manager.dart
@@ -0,0 +1,5 @@
+class WidgetConstants {
+  static const taskIdentifier = 'medito-widget-update';
+  static const taskName = 'updateWidget';
+  static const widgetGroupId = 'group.org.medito.widget';
+}
