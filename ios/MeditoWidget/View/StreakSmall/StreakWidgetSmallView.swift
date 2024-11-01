ff96c3a65ce9fd45a191b0afa9a50b45961a6cb9
Luz Ams <luzracca@gmail.com>
2024-11-01 10:58:31 +0100

    Added previews for "Small" widget

--- ios/MeditoWidget/View/StreakSmall/StreakWidgetSmallView.swift
+++ ios/MeditoWidget/View/StreakSmall/StreakWidgetSmallView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import WidgetKit
 
 struct StreakWidgetSmallView: View {
     var entry: MeditoTimelineProvider.Entry
@@ -18,5 +19,42 @@ struct StreakWidgetSmallView: View {
                 .foregroundColor(Color.accentPurple)
         }
         .widgetBackground(Color.deepBlue)
+    }
+}
+
+struct StreakWidgetSmallView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            // Done today - active streak
+            StreakWidgetSmallView(entry: MeditoWidgetEntry(
+                date: Date(),
+                streakValue: 7,
+                audioCompleted: [
+                    Date().timeIntervalSince1970 * 1000  // today
+                ]
+            ))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewDisplayName("Done Today")
+            
+            // Not done today
+            StreakWidgetSmallView(entry: MeditoWidgetEntry(
+                date: Date(),
+                streakValue: 6,
+                audioCompleted: [
+                    Date().addingTimeInterval(-86400).timeIntervalSince1970 * 1000  // yesterday
+                ]
+            ))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewDisplayName("Not Done Today")
+            
+            // Zero streak
+            StreakWidgetSmallView(entry: MeditoWidgetEntry(
+                date: Date(),
+                streakValue: 0,
+                audioCompleted: []
+            ))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewDisplayName("Zero Streak")
+        }
     }
 }
