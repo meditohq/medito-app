9c94460e65a05ce8dc7b62da3b2921a279c3ae7d
Michael Speed <michaelcspeed@gmail.com>
2024-11-20 16:54:15 +0100

    Fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ import 'package:home_widget/home_widget.dart';
 import 'package:medito/constants/work_manager.dart';
 import 'package:medito/providers/device_and_app_info/device_and_app_info_provider.dart';
 import 'package:medito/providers/notification/reminder_provider.dart';
-import 'package:medito/constants/http/http_constants.dart';
 import 'package:medito/providers/player/audio_state_provider.dart';
 import 'package:medito/providers/player/player_provider.dart';
 import 'package:medito/providers/shared_preference/shared_preference_provider.dart';
@@ -29,9 +28,6 @@ import 'package:workmanager/workmanager.dart';
 
 import 'constants/theme/app_theme.dart';
 import 'firebase_options.dart';
-import 'package:medito/providers/device_and_app_info/device_and_app_info_provider.dart';
-import 'package:medito/providers/notification/reminder_provider.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:medito/repositories/auth/auth_repository.dart';
 
 final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
