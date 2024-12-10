363dd14c717bbe17e6ef91d1d61ef76d4bdfc085
Michael Speed <michaelcspeed@gmail.com>
2024-12-10 16:45:21 +0100

    Refresh token logging

--- lib/services/auth/auth_interceptor.dart
+++ lib/services/auth/auth_interceptor.dart
@@ -107,6 +107,11 @@ class AuthInterceptor extends Interceptor with RetryMixin {
 
   Future<String?> _refreshToken() async {
     final session = await Supabase.instance.client.auth.refreshSession();
+    
+    if (session.session?.accessToken != null) {
+      debugPrint('Token refreshed successfully');
+    }
+    
     return session.session?.accessToken;
   }
 
