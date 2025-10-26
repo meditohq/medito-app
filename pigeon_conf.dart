c88c31115b7b929bd318b53dd1cd318bcc675c68
Michael Speed <michaelcspeed@gmail.com>
2025-10-26 17:46:53 +0100

    3.5.14+30154

--- pigeon_conf.dart
+++ pigeon_conf.dart
@@ -6,7 +6,7 @@ import 'package:pigeon/pigeon.dart';
 @ConfigurePigeon(
   PigeonOptions(
     dartOut: 'lib/src/audio_pigeon.g.dart',
-    dartOptions: DartOptions(),
+    dartOptions: DartOptions(dartPackageName: 'medito'),
     kotlinOut:
         'android/app/src/main/kotlin/meditofoundation/medito/pigeon/AudioPigeon.g.kt',
     kotlinOptions: KotlinOptions(package: 'meditofoundation.medito.pigeon'),
