e9ddd8d7f0f2dc0a15478b075fbdefdc49f2c4e9
Michael Speed <michaelcspeed@gmail.com>
2025-11-06 12:23:35 +0100

    Fix images

--- lib/constants/http/http_constants.dart
+++ lib/constants/http/http_constants.dart
@@ -159,9 +159,7 @@ class HTTPConstants {
 
   // DEAD DOMAINS - domains that no longer exist and should be avoided
   static const List<String> _deadDomains = [
-    'images.medito.space',
-    'cdn.medito.app',
-    // Add other dead domains here as they are discovered
+    'images.medito.space',  
   ];
 
   /// Checks if a URL is from a dead/unavailable domain
