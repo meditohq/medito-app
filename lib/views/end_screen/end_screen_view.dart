521a5a1a3c5fa26d4a82530bb5129fdde979020b
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2026-03-03 14:32:15 +0000

    Upgrade haptic feedback (#705)
    * Upgrade haptic_feedback
    
    * Set HapticsUsage.media to fix missing haptics for users with disabled Android touch haptics
    
    ---------
    
    Co-authored-by: Michael Speed <michaelcspeed@users.noreply.github.com>

--- lib/views/end_screen/end_screen_view.dart
+++ lib/views/end_screen/end_screen_view.dart
@@ -157,7 +157,7 @@ class _EndScreenViewState extends ConsumerState<EndScreenView>
     _hasFiredHapticFeedback = true;
     final canVibrate = await Haptics.canVibrate();
     if (canVibrate) {
-      await Haptics.vibrate(HapticsType.success);
+      await Haptics.vibrate(HapticsType.success, usage: HapticsUsage.media);
     }
   }
 
