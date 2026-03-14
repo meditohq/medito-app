cd695ef3c0b62644536a8fbcd8c002f907d9029d
Michael Speed <michaelcspeed@users.noreply.github.com>
2026-03-14 16:45:08 +0100

    Update homescreen package

--- android/app/src/main/kotlin/meditofoundation/medito/widget/MeditationWidgetReceiver.kt
+++ android/app/src/main/kotlin/meditofoundation/medito/widget/MeditationWidgetReceiver.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.glance.appwidget.updateAll
-import HomeWidgetGlanceWidgetReceiver
+import es.antonborri.home_widget.HomeWidgetGlanceWidgetReceiver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
