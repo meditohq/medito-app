86cc97443b8ccf331331200965e2d2b1d66ec1ba
Michael Speed <michaelcspeed@users.noreply.github.com>
2026-03-16 11:36:25 +0100

    Fix deep link complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,6 @@
 import 'dart:async';
 import 'dart:io';
 
-// Completes when the app has finished auth/init and landed on the main screen.
-// Deep link navigation waits for this before pushing any route.
-final appReadyCompleter = Completer<void>();
-
 import 'package:audio_service/audio_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
