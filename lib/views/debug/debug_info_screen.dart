c44b31ac1c63614c9bc9687f59f6f95f38746358
Michael Speed <michaelcspeed@users.noreply.github.com>
2026-04-13 19:09:34 +0200

    Fix base64

--- lib/views/debug/debug_info_screen.dart
+++ lib/views/debug/debug_info_screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_initializing_formals
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -104,8 +103,7 @@ class DebugInfoScreen extends ConsumerWidget {
     final stats = ref.read(statsProvider).asData?.value;
     if (stats == null) return '';
     final bytes = utf8.encode(jsonEncode(stats.toJson()));
-    final compressed = GZipCodec().encode(bytes);
-    return base64Encode(compressed);
+    return base64Encode(bytes);
   }
 
   Widget _buildInfoView(
